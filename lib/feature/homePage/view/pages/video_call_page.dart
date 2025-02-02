a61fd89e3e25d8bf23f50827bbd346eb29e05959
AdnanAdu <adnanpe5@gmail.com>
2025-02-02 16:44:53 +0530

    added Muted audio and video Switches

--- lib/feature/homePage/view/pages/video_call_page.dart
+++ lib/feature/homePage/view/pages/video_call_page.dart
@@ -2,16 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:zoom_clone/core/services/auth_services.dart';
 import 'package:zoom_clone/core/theme_data/colors.dart';
+import 'package:zoom_clone/feature/homePage/view/widgets/meeting_option_widget.dart';
 
 class VideoCallPage extends HookWidget {
   const VideoCallPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //Controllers
     final AuthServices authServices = AuthServices();
     final TextEditingController meetinIdController = useTextEditingController();
     final TextEditingController nameController =
         useTextEditingController(text: authServices.user.displayName ?? 'user');
+
+    //states
+    final onAudioMute = useState<bool>(true);
+    final onVideoMute = useState<bool>(true);
+
+    ///Function
+    ///onAudioMute
+
+    isAudioMuted(bool val) {
+      onAudioMute.value = val;
+    }
+
+    ///onVideoMute
+    isVideoMuted(bool val) {
+      onVideoMute.value = val;
+    }
+
+    ///Join Meeting
+    joinMeeting() {}
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -25,7 +47,7 @@ class VideoCallPage extends HookWidget {
       body: Column(
         children: [
           SizedBox(
-            height: 16,
+            height: 60,
             child: TextField(
               controller: meetinIdController,
               maxLines: 1,
@@ -41,7 +63,7 @@ class VideoCallPage extends HookWidget {
             ),
           ),
           SizedBox(
-            height: 16,
+            height: 60,
             child: TextField(
               controller: nameController,
               maxLines: 1,
@@ -55,6 +77,32 @@ class VideoCallPage extends HookWidget {
                 contentPadding: const EdgeInsets.fromLTRB(16, 8, 0, 0),
               ),
             ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: joinMeeting,
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                "Join",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          MeetingOptionWidget(
+            text: "Mute Audio",
+            isMute: onAudioMute.value,
+            onChanged: isAudioMuted,
+          ),
+          MeetingOptionWidget(
+            text: "Mute Video",
+            isMute: onVideoMute.value,
+            onChanged: isVideoMuted,
           ),
         ],
       ),
